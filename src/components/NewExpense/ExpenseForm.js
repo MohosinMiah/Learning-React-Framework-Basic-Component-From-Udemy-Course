3588e1761b5091998a926d31453d9a0506762f69
mohosin16103308 <hamza1610330816@gmail.com>
2022-04-26 22:14:30 +0600

    Handling Form Data using useState for Each input field

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,43 @@
-import './ExpenseForm'
+import { useState } from 'react';
+import './ExpenseForm.css';
+
 const ExpenseForm = ()=>{
+
+    const [ enteredTitle, setEnteredTitle ] = useState('');
+    const [ enteredAmount, setEnteredAmount ] = useState('');
+    const [ enteredDate, setEnteredDate ] = useState('');
+    
+    const titleChangeHandler = (event) =>{
+        setEnteredTitle(event.target.value);
+    }
+    const amontChangeHandler = (event) =>{
+        setEnteredAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) =>{
+        setEnteredDate(event.target.value);
+    }
+
     return(
         <div> 
             <form  className="new-expense__controls"> 
+            <div className="new-expense__controls">
             <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"></input>
+                    <input type="text" onChange={titleChangeHandler} ></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"></input>
+                    <input type="number" min="0.01" step="0.01"  onChange={amontChangeHandler}  ></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-12" max="2021-01-12"></input>
+                    <input type="date" min="2019-01-12" max="2021-01-12"  onChange={dateChangeHandler}  ></input>
                 </div>
                 <div className="new-expense__actions">
                 <input type="submit" value="Submit" />
                 </div>
     
+            </div>
 
         </form>
         </div>
